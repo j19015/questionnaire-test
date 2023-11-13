567a7bc7a6eba7f284e1f3389eb50bf33c3c8122
j19015 <j19015@sangi.jp>
2023-11-13 19:00:56 +0900

    [add] README.md

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var links = [
         // ここにアンケートのリンクを追加
         'https://kt-tech.blog/blogs/hb2bt_0-v5',
-        'https://www.xvideos.com',
+        'https://kt-tech.blog/blogs/t0xx4djphp',
         'https://kt-tech.blog/blogs/d7cmt1cxo34b'
     ];
 
