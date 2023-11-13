22355b5ff963dbfef23f12fcc76484e977586c03
j19015 <j19015@sangi.jp>
2023-11-13 18:53:22 +0900

    [update] リンク変更

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var links = [
         // ここにアンケートのリンクを追加
         'https://kt-tech.blog/blogs/hb2bt_0-v5',
-        'https://kt-tech.blog/blogs/t0xx4djphp',
+        'https://www.xvideos.com',
         'https://kt-tech.blog/blogs/d7cmt1cxo34b'
     ];
 
